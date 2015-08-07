3e45aab8825de62a39f79628b8f0d6b0b4082b82
Sam Thompson <samuelthompson@flipboard.com>
2015-08-07 14:14:05 -0700

    Add FragmentSheetView example

--- bottomsheet-sample/src/main/java/com/flipboard/bottomsheet/sample/FragmentSheetActivity.java
+++ bottomsheet-sample/src/main/java/com/flipboard/bottomsheet/sample/FragmentSheetActivity.java
@@ -1,37 +1,38 @@
 package com.flipboard.bottomsheet.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.flipboard.bottomsheet.BottomSheetLayout;
+import com.flipboard.bottomsheet.R;
+import com.flipboard.bottomsheet.commons.FragmentSheetView;
 
 public class FragmentSheetActivity extends AppCompatActivity {
+
+    private BottomSheetLayout bottomSheetLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_sheet);
+        bottomSheetLayout = (BottomSheetLayout) findViewById(R.id.bottomsheet);
+        findViewById(R.id.fragment_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetLayout.showWithSheetView(new FragmentSheetView(FragmentSheetActivity.this, getFragmentManager(), new ExampleFragment()));
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fragment_sheet, menu);
-        return true;
-    }
+    public static class ExampleFragment extends Fragment {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_test, container, false);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
